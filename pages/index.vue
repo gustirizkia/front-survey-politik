d046b213beb35b5cf7de126e7d93f417da369297
gustirizkia <gustidoang936@gmail.com>
2022-10-07 23:00:18 +0700

    change update

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,15 @@
     <Bottombar />
     <div class="bg-gray-400">
       <div class="md:w-1/3 bg-white min-h-screen md:mx-auto px-3 pt-3">
-        <div class="">
+        <div class="flex">
           <img
             src="https://images.unsplash.com/photo-1664574652984-5b5f769bef07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMDB8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
             alt=""
-            class="w-12 h-12 object-cover rounded-full"
+            class="w-12 h-12 object-cover rounded-lg"
           />
+          <div class="text-gray-700 font-semibold ml-3 text-xl my-auto">
+            Gusti
+          </div>
         </div>
       </div>
     </div>
